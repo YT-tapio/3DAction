a8dc3a372728b3d5d73b1f8ea817e15681cecda0
YT-tapio <Ytappii175773@icloud.com>
2025-12-20 00:20:25 +0900

    dxlibとeffekserの導入をしました。

---
+++ source/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include"DxLib.h"
+#include"EffekseerForDXLib.h"
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+    if (DxLib_Init() == -1)    // ＤＸライブラリ初期化処理
+    {
+        return -1;    // エラーが起きたら直ちに終了
+    }
+
+
+
+    DxLib_End();        // ＤＸライブラリ使用の終了処理
+
+    return 0;        // ソフトの終了
+}
