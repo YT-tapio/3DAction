f1b17ef88868695cfc09bc8d4b31ce6c209e60b1
YT-tapio <Ytappii175773@icloud.com>
2026-05-25 01:56:30 +0900

    statusのstructを作った。

---
+++ source/status.h
@@ -0,0 +1,11 @@
+#pragma once
+
+struct Status
+{
+	float hp;
+	float physical_atk;	// 物理攻撃力
+	float magic_atk;		// 魔法攻撃力
+	float physical_def;	// 物理防御力
+	float magic_def;		// 魔法防御力
+	float stamina;			// スタミナ
+};
